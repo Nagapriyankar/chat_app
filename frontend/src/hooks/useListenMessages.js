6109530cf6f955b0351ced80c3bc93a6e153afa2
Nagapriyankar <nagapriyankar@gmail.com>
2024-04-26 08:49:50 +0530

    message listener - notification added

--- frontend/src/hooks/useListenMessages.js
+++ frontend/src/hooks/useListenMessages.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import { useSocketContext } from '../context/SocketCotext'
 import useConversation from '../zustand/useConversation'
-
+import notificationSound from "../assets/sounds/notification.mp3"
 //message listener - receivers get the message immediately without need to refresh
 
 const useListenMessages = () => {
@@ -12,9 +12,12 @@ const useListenMessages = () => {
         //listen for new message event 
         socket?.on("newMessage", (newMessage) => {
             newMessage.shouldShake = true
+            const sound = new Audio(notificationSound)
+            sound.play()
             setMessages([...messages, newMessage])
         })
 
+        //prevent listening to the event more than once
         return () => socket.off("newMessage")
     }, [socket, setMessages, messages])
 }
