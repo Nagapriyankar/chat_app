e0cd8a3c678c41d69a57d83dddbbfcbe73bb3284
Nagapriyankar <nagapriyankar@gmail.com>
2024-04-27 16:20:08 +0530

    deployment changes

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,20 @@
 import express from "express"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
+import path from "path"
 
 import authRoutes from "./routes/authRoutes.js"
-import connectToMongo from "./middleware/connectToMongo.js"
 import messageRoute from "./routes/messageRoute.js"
 import userRoute from "./routes/userRoute.js"
+
+import connectToMongo from "./middleware/connectToMongo.js"
 import { app, server } from "./socket/socket.js"
 
 
 //const app = express()  //express app is created in socket.io
 const PORT = process.env.PORT || 5000
+
+const __dirname = path.resolve()
 
 dotenv.config()
 
@@ -21,6 +25,19 @@ app.use(cookieParser())
 app.use("/api/auth", authRoutes)
 app.use("/api/conversation", messageRoute)
 app.use("/api/users", userRoute)
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+})
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")))   //static middleware to use static files like html, css
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+
+})
 
 //check root route
 app.get("/", (req, res) => {
